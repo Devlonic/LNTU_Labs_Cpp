7a08a38d3cb9af28d2c76197347dada407073541
Devloner <tminecraftben@gmail.com>
2024-03-15 14:42:47 +0200

    done

--- StudentList/source.cpp
+++ StudentList/source.cpp
@@ -2,8 +2,29 @@
 #include <fstream>
 #include <vector>
 #include <string.h>
-
+#include "libbmp.h"
 using namespace std;
 
 int main() {
+	int size = 0;
+	cout << "Enter size: ";
+	cin >> size;
+	BmpImg img(size, size);
+	srand(time(0));
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j < size; j++)
+		{
+			img.set_pixel(j, i, 0xff, 0xff, 0xff);
+		}
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t j = 0; j <= i; j++)
+		{
+			img.set_pixel(j, i, rand() % 0xff, rand() % 0xff, rand() % 0xff);
+		}
+	}
+	img.write("my.bmp");
 }
